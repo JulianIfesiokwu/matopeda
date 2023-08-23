bd867df8de5633b05e20bc1ee631c59d185d8d44
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2023-08-23 14:42:09 +0100

    Feat: added eslint to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
-      'Inter': ['Inter', 'sans-serif'],
-      'Noto': ['Noto Serif', 'serif'],
+      Inter: ["Inter", "sans-serif"],
+      Noto: ["Noto Serif", "serif"],
     },
     extend: {
       backgroundImage: {
-        'hero-pattern': "url('/src/assets/homepage.jpg')",
-      }
+        "hero-pattern": "url('/src/assets/homepage.jpg')",
+      },
     },
   },
   plugins: [],
-}
-
+};
