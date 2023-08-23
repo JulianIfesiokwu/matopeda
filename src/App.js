96984ebcad6c1274210f00fd5c617554458af171
EGBUNA JULIAN <julianifesiokwu@gmail.com>
2023-08-23 14:45:21 +0100

    Feat: updated router to react router v6.4

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
-import Layout from './components/Layout/Layout';
-import Homepage from './pages/Homepage';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Homepage from "./pages/Homepage";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<Layout />}>
         <Route index path="/" element={<Homepage />} />
-      </Route>
-    )
-  )
+      </Route>,
+    ),
+  );
 
   return (
-    <div className='app'>
+    <div className="app">
       <RouterProvider router={router} />
     </div>
   );
